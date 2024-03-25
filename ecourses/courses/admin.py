2eec6e3b739b552626e3d134d7d193377ae6020e
Your name <mtrunghieu12@gmail.com>
2024-03-26 00:09:37 +0700

    Trong các lesson có nhiều tag và ngược lại dùng TabularInline

--- ecourses/courses/admin.py
+++ ecourses/courses/admin.py
@@ -3,7 +3,7 @@ from django.utils.html import mark_safe
 from django import forms
 from ckeditor_uploader.widgets \
     import CKEditorUploadingWidget
-from .models import Course, Category, Lesson
+from .models import Course, Category, Lesson, Tag
 
 
 class LessonForm(forms.ModelForm):  # tạo ra 1 form có thanh cong cụ như word
@@ -14,27 +14,39 @@ class LessonForm(forms.ModelForm):  # tạo ra 1 form có thanh cong cụ như w
         fields = '__all__'
 
 
+class LessonTagInlineAdmin(admin.TabularInline):  # trong Lesson có nhiều tag, add các tag liên quan VÀ TRONG CÁC TAG CÓ NHIỀU LESSON
+    model = Lesson.tags.through
+
+
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['id', 'subject', 'created_date', 'course']
     list_filter = ['subject', 'created_date']
     search_fields = ['subject', 'course__subject']
     readonly_fields = ["avatar"]
     form = LessonForm
+    inlines = [LessonTagInlineAdmin, ]
 
     def avatar(self, lesson):
         return mark_safe('''
         <img src='/static/{img_url}' alt='{alt}' width='120px' />
         '''.format(img_url=lesson.image.name, alt=lesson.subject))
 
+
 class LessonInlineAdmin(admin.StackedInline):
     model = Lesson
-    fk_name = 'course' #tên khóa ngoại trong Models Lesson có course
+    fk_name = 'course'  # tên khóa ngoại trong Models Lesson có course
+
 
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInlineAdmin, ]
 
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [LessonTagInlineAdmin]
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Course, CourseAdmin)#trong khóa học hện các bài học liên quan
+admin.site.register(Course, CourseAdmin)  # trong khóa học hện các bài học liên quan
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Tag,TagAdmin)
