1610d502056d8beee0b15ba41b1fd61ba4e5d868
Your name <mtrunghieu12@gmail.com>
2024-03-27 22:04:47 +0700

    Tạo URLConfigs để khi /domainName sẽ render ra views

--- ecourses/courses/urls.py
+++ ecourses/courses/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from .admin import admin_site
 
 urlpatterns = [
     path('',views.index, name="index"),
+    path('welcome/<int:year>', views.WELCOME), #'welcome': định nghĩa url, còn WELCOME: render trang đó lên
+    re_path(r'^welcome2/(?P<year>[0-9]{1,2})/$', views.WELCOME2),#biểu thức chính quy 're_path'
     path('admin/', admin_site.urls)
 ]
--- ecourses/courses/views.py
+++ ecourses/courses/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def index(request):
-     return HttpResponse("Hello world!!!!")
+    return HttpResponse("Hello world!!!!")
+
+
+def WELCOME(request, year):  # tham số year được truyền từ urls của app
+    return HttpResponse("WELCOME" + str(year))
+
+
+def WELCOME2(request, year):
+    return HttpResponse("WELCOME " + str(year))
