033825f3fe0dd07c5474fc82deef98483b974f2e
Your name <mtrunghieu12@gmail.com>
2024-04-05 22:21:48 +0700

    Phân trang bằng paginator

--- ecourses/courses/views.py
+++ ecourses/courses/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from rest_framework import viewsets,generics
 from .models import Course
 from .serializers import CourseSerializer
+from courses import serializers, paginators
 
 class CourseViewSet(viewsets.ViewSet,generics.ListAPIView):
     queryset = Course.objects.filter(active=True)
     serializer_class = CourseSerializer
+    pagination_class = paginators.CoursePaginator
 
 def index(request):
     return HttpResponse("Hello world!!!!")
