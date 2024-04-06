e15c357f34bb51b8d7a1387e07448b6210f609fa
Your name <mtrunghieu12@gmail.com>
2024-04-06 22:23:17 +0700

    Lấy danh sách các bài học của 1 Course khi truyền id của Course vào

--- ecourses/courses/views.py
+++ ecourses/courses/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets, generics
-from .models import Course,Lesson
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 from courses import serializers, paginators
 
@@ -11,15 +14,22 @@ class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
     serializer_class = CourseSerializer
     pagination_class = paginators.CoursePaginator
 
+    @action(methods=['get'], detail=True)
+    def get_lessons(self, request, pk):  # pk: id truyền vào
+        lessons = self.get_object().lessons.filter(
+            active=True).all()  # .lessons. này là khóa ngoại của course có related_name
+
+        return Response(serializers.LessonSerializer(lessons, many=True).data)  # trả về nhiều nên many=True
+
 
 class LessonViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Lesson.objects.filter(active=True).all()
     serializer_class = LessonSerializer
 
     def get_queryset(self):
-        queries = self.queryset #lấy những bài học đã active
+        queries = self.queryset  # lấy những bài học đã active
 
-        q = self.request.query_params.get("q")# khi search /?q=... thì lấy giá trị q về
+        q = self.request.query_params.get("q")  # khi search /?q=... thì lấy giá trị q về
         if q:
             queries = queries.filter(subject__icontains=q)
 
